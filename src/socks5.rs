8b917f0b6bcfe9ff543d2dbfe51576fb8c0fa5c9
Anton Mikhalev <compromisedbeacon+gh@gmail.com>
2018-05-18 11:59:42 +0300

    Use std::io::Error.
    Hyper allows only std::io::Error in custom connector implementations,
    which, unfortunately, makes using custom error types impossible.

--- src/socks5.rs
+++ src/socks5.rs
@@ -2,7 +2,6 @@ use std::{self, io};
 use std::convert::From;
 use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
 
-use failure::{self, Fail};
 use futures::prelude::*;
 use futures::{self, Future, future, Poll, Stream};
 
@@ -27,7 +26,7 @@ impl Socks5Tunnel {
         proxy_addr: &SocketAddr,
         server_addr: &TunnelDestAddr,
         handle: &Handle
-    ) -> impl Future<Item = TcpStream, Error = failure::Error> {
+    ) -> impl Future<Item = TcpStream, Error = io::Error> {
         // This is function is just a scaffolding over a real one.
         // futures-await does not support using parameters passed as
         // references, so we do all our copying here.
@@ -42,11 +41,11 @@ impl Socks5Tunnel {
         proxy_addr: SocketAddr,
         server_addr: TunnelDestAddr,
         handle: Handle
-    ) -> Result<TcpStream, failure::Error> {
+    ) -> Result<TcpStream, io::Error> {
         // Establish connection with proxy server, and negotiate auth method.
-        let stream = Socks5Tunnel::lift_error(
-            await!(TcpStream::connect(&proxy_addr, &handle))
-        )?;
+        let stream = await!(TcpStream::connect(&proxy_addr, &handle)).map_err(|err| {
+            wrap_io_err("unable to connect to proxy server ({})", err)
+        })?;
 
         // We do not have support for the authentication ATM, request a free
         // pass from the server.
@@ -60,17 +59,18 @@ impl Socks5Tunnel {
             read_exact(s, [0, 0])
         });
 
-        let (stream, handshake_resp) = Socks5Tunnel::lift_error(
-            await!(handshaking)
-        )?;
+        let (stream, handshake_resp) = await!(handshaking).map_err(|err| {
+            wrap_io_err("proxy handshake failure ({})", err)
+        })?;
 
         // First byte in the response is protocol version.
         // Second byte is the auth method the server chose.
         if handshake_resp[1] != 0x00 {
             let msg = format!(
-                "Proxy server refused to accept our auth method (0x{:X})",
-                handshake_resp[1]);
-            return Err(Socks5Error::GeneralFailure(msg).into());
+                "proxy did not accept our auth method (0x{:02X})",
+                handshake_resp[1]
+            );
+            return Err(other_err(&msg));
         }
 
         // Request proxy server to setup a TCP tunnel to a remote server.
@@ -85,9 +85,9 @@ impl Socks5Tunnel {
         let message = Socks5Tunnel::make_connect_message(&server_addr)?;
         let tunneling = write_all(stream, message)
             .and_then(|(s, _)| read_exact(s, [0; 5]));
-        let (stream, response_preamble) = Socks5Tunnel::lift_error(
-            await!(tunneling)
-        )?;
+        let (stream, response_preamble) = await!(tunneling).map_err(|err| {
+            wrap_io_err("proxy handshake failure ({})", err)
+        })?;
 
         Socks5Tunnel::check_socks5_error_code(response_preamble[1])?;
 
@@ -105,26 +105,24 @@ impl Socks5Tunnel {
             Socks5Tunnel::FIELD_ADR_HOSTNAME => {
                 read_exact(stream, vec![0; response_preamble[4] as usize])
             },
-            _ => return Err(Socks5Error::GeneralFailure(
-                    "Unexpected address type".to_owned()).into()),
+            _ => {
+                let msg = format!(
+                    "proxy response contains unsupported BND.ADDR (0x{:02X})",
+                    response_preamble[3]
+                );
+                return Err(other_err(&msg));
+            }
         };
 
-        let (stream, _whatever) = Socks5Tunnel::lift_error(
-            await!(discarding)
-        )?;
+        let (stream, _whatever) = await!(discarding).map_err(|err| {
+            wrap_io_err("proxy handshake failure ({})", err)
+        })?;
         Ok(stream)
-    }
-
-    fn lift_error<T>(t: Result<T, io::Error>) -> Result<T, Socks5Error> {
-        match t {
-            Ok(x) => Ok(x),
-            Err(err) => Err(Socks5Error::ProxyConnectionFailed(err).into()),
-        }
     }
 
     fn make_connect_message(
         server_addr: &TunnelDestAddr
-    ) -> Result<Vec<u8>, Socks5Error> {
+    ) -> Result<Vec<u8>, io::Error> {
         let mut message = Vec::<u8>::with_capacity(32);
 
         // Message preamble.
@@ -151,8 +149,7 @@ impl Socks5Tunnel {
                 message.push(Socks5Tunnel::FIELD_ADR_HOSTNAME);
 
                 if h.len() > u8::max_value().into() {
-                    let msg = "Hostname length exceeds 255 characters";
-                    return Err(Socks5Error::GeneralFailure(msg.into()).into());
+                    return other_err("hostname is too long");
                 }
 
                 // Hostname is preceded by its length.
@@ -169,41 +166,35 @@ impl Socks5Tunnel {
         Ok(message)
     }
 
-    fn check_socks5_error_code(errc: u8) -> Result<(), Socks5Error> {
+    fn check_socks5_error_code(errc: u8) -> Result<(), io::Error> {
         if errc == 0 {
             return Ok(());
         }
 
-        let msg = match errc {
-            3 => Some("Remote host's network is unreachable"),
-            4 => Some("Remote host is unreachable"),
-            5 => Some("Remote host has refused connection"),
+        let err = match errc {
+            3 => Some("proxy reports that host's network is unreachable"),
+            4 => Some("proxy reports that host is unreachable"),
+            5 => Some("proxy reports that host has refused connection"),
             _ => None,
         };
-        if msg.is_some() {
-            return Err(Socks5Error::RemoteConnectionFailed(
-                msg.unwrap().into()).into());
+        if err.is_some() {
+            let msg = format!(
+                "proxy server was unable to connect to destination host ({})",
+                err
+            );
+            return Err(other_err(&msg));
         }
-        let msg = match errc {
-            1 => Some("General proxy server failure"),
-            2 => Some("Proxy server refused to allow access"),
-            6 => Some("TTL expired"),
-            7 => Some("Command not supported"),
-            8 => Some("Address type not supported"),
-            _ => Some("Proxy server returned an unknown error"),
+        let err = match errc {
+            1 => Some("general proxy server failure"),
+            2 => Some("proxy server refused to allow access"),
+            6 => Some("ttl expired"),
+            7 => Some("command not supported"),
+            8 => Some("address type not supported"),
+            _ => Some("unknown error"),
         }.unwrap().to_owned();
-        return Err(Socks5Error::GeneralFailure(msg).into());
+        let msg = format!("proxy server returned an error ({})", err);
+        return Err(other_err(&msg));
     }
-}
-
-#[derive(Debug, Fail)]
-pub enum Socks5Error {
-    #[fail(display = "Coudln't connect to proxy server")]
-    ProxyConnectionFailed(#[cause] io::Error),
-    #[fail(display = "Proxy server is unable to reach remote server ({})", _0)]
-    RemoteConnectionFailed(String),
-    #[fail(display = "General proxy server failure ({})", _0)]
-    GeneralFailure(String),
 }
 
 #[derive(Clone)]
@@ -222,4 +213,14 @@ impl<'a> From<(&'a str, u16)> for TunnelDestAddr {
     fn from(t: (&'a str, u16)) -> TunnelDestAddr {
         TunnelDestAddr::Hostname(t.0.into(), t.1)
     }
+}
+
+fn wrap_io_err(msg: &str, err: io::Error) -> io::Error {
+    let kind = err.kind();
+    let msg = format!("{} ({})", msg, err);
+    io::Error::new(kind, msg)
+}
+
+fn other_err(msg: &str) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, msg)
 }
